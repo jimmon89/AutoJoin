895de3b15ed61b8275ffc145f5fc63e8213c8faf
joserobjr <jose.rob.jr@gmail.com>
2016-11-08 19:16:31 -0300

    Fix version number

--- src/main/java/com/mcf/davidee/autojoin/AutoJoin.java
+++ src/main/java/com/mcf/davidee/autojoin/AutoJoin.java
@@ -30,7 +30,7 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToSe
 public class AutoJoin {
 	
 	public static final int PROTOCOL_VER = 5;
-	public static final String VERSION = "1.7.10.0";
+	public static final String VERSION = "1.8.8.0";
 	
 	@Instance("AutoJoin")
 	public static AutoJoin instance;
