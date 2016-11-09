0afeef1b88f53a6ca41c5935720951eb852ae6f0
joserobjr <jose.rob.jr@gmail.com>
2016-11-09 05:34:03 -0300

    Updated to Minecraft 1.10.2

--- src/main/java/com/mcf/davidee/autojoin/AutoJoin.java
+++ src/main/java/com/mcf/davidee/autojoin/AutoJoin.java
@@ -27,7 +27,7 @@ import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToSe
 public class AutoJoin {
 	
 	public static final int PROTOCOL_VER = 210;
-	public static final String VERSION = "1.10.0.0";
+	public static final String VERSION = "1.10.2.0";
 	
 	@Instance("AutoJoin")
 	public static AutoJoin instance;
